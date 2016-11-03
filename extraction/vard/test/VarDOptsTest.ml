a80dc5b86f11c56c295c9a2c3e6b02ea3129878a
Karl Palmskog <palmskog@gmail.com>
2016-11-03 18:47:50 -0500

    organize ounit tests better

--- extraction/vard/test/VarDOptsTest.ml
+++ extraction/vard/test/VarDOptsTest.ml
@@ -1,7 +1,8 @@
+open ListLabels
 open OUnit2
 
-let parse_line line =
-  let listl = (Str.split (Str.regexp " ") line) in
+let arr_of_string s =
+  let listl = (Str.split (Str.regexp " ") s) in
   (Array.of_list listl)
 
 let tear_down () text_ctxt =
@@ -11,31 +12,34 @@ let tear_down () text_ctxt =
   VarDOpts.dbpath := VarDOpts.dbpath_default;
   VarDOpts.debug := VarDOpts.debug_default
 
-let parse_correct_line test_ctxt =
-  VarDOpts.parse (parse_line "./vard.native -dbpath /tmp/vard-8000 -me 0 -port 8000 -node 0,localhost:9000 -node 1,localhost:9001 -node 2,localhost:9002");
+let test_parse_correct_line test_ctxt =
+  VarDOpts.parse (arr_of_string "./vard.native -dbpath /tmp/vard-8000 -me 0 -port 8000 -node 0,localhost:9000 -node 1,localhost:9001 -node 2,localhost:9002");
   assert_equal 0 !VarDOpts.me;
   assert_equal 8000 !VarDOpts.port;
   assert_equal [(0, ("localhost", 9000)); (1, ("localhost", 9001)); (2, ("localhost", 9002))] !VarDOpts.cluster;
   assert_equal "/tmp/vard-8000" !VarDOpts.dbpath;
   assert_equal false !VarDOpts.debug
 
-let validate_correct_line test_ctxt =
-  VarDOpts.parse (parse_line "./vard.native -dbpath /tmp/vard-8000 -me 0 -port 8000 -node 0,localhost:9000 -node 1,localhost:9001 -node 2,localhost:9002");
+let test_validate_correct_line test_ctxt =
+  VarDOpts.parse (arr_of_string "./vard.native -dbpath /tmp/vard-8000 -me 0 -port 8000 -node 0,localhost:9000 -node 1,localhost:9001 -node 2,localhost:9002");
   assert_equal () (VarDOpts.validate ())
 
-let validate_missing_me text_ctxt =
-  VarDOpts.parse (parse_line "./vard.native -dbpath /tmp/vard-8000 -port 8000 -node 0,localhost:9000 -node 1,localhost:9001 -node 2,localhost:9002");
+let test_validate_missing_me text_ctxt =
+  VarDOpts.parse (arr_of_string "./vard.native -dbpath /tmp/vard-8000 -port 8000 -node 0,localhost:9000 -node 1,localhost:9001 -node 2,localhost:9002");
   assert_raises (Arg.Bad "Please specify the node name -me") VarDOpts.validate
+
+let test_list =
+  ["parse correct line", test_parse_correct_line;
+   "validate correct line", test_validate_correct_line;
+   "validate missing me", test_validate_missing_me;
+  ]
   
+let test_suite =
+  "VarDOpts" >:::
+    (map test_list ~f:(fun (name, test_fn) ->
+      name >:: (fun test_ctxt ->
+	bracket ignore tear_down test_ctxt;
+	test_fn test_ctxt)
+     ))
 
-let suite = "vard" >:::
-[
-  "parse correct line" >:: (fun test_ctxt -> bracket ignore tear_down test_ctxt; parse_correct_line test_ctxt);
-
-  "validate correct line" >:: (fun test_ctxt -> bracket ignore tear_down test_ctxt; validate_correct_line test_ctxt);
-  
-  "validate missing me" >:: (fun test_ctxt -> bracket ignore tear_down test_ctxt; validate_missing_me test_ctxt);
- 
-]
-
-let _ = run_test_tt_main suite
+let _ = run_test_tt_main test_suite
