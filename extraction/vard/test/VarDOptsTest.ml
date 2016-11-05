22b6c11e09a80023ecceab12c132a1d9a79b8365
Karl Palmskog <palmskog@gmail.com>
2016-11-04 20:40:35 -0500

    more VarDOpts tests

--- extraction/vard/test/VarDOptsTest.ml
+++ extraction/vard/test/VarDOptsTest.ml
@@ -28,10 +28,20 @@ let test_validate_missing_me text_ctxt =
   VarDOpts.parse (arr_of_string "./vard.native -dbpath /tmp/vard-8000 -port 8000 -node 0,localhost:9000 -node 1,localhost:9001 -node 2,localhost:9002");
   assert_raises (Arg.Bad "Please specify the node name -me") VarDOpts.validate
 
+let test_validate_empty_cluster text_ctxt =
+  VarDOpts.parse (arr_of_string "./vard.native -dbpath /tmp/vard-8000 -me 0 -port 8000");
+  assert_raises (Arg.Bad "Please specify at least one -node") VarDOpts.validate
+
+let test_validate_me_not_cluster_member text_ctxt =
+  VarDOpts.parse (arr_of_string "./vard.native -dbpath /tmp/vard-8000 -me 0 -port 8000 -node 1,localhost:9001 -node 2,localhost:9002");
+  assert_raises (Arg.Bad "0 is not a member of this cluster") VarDOpts.validate
+
 let test_list =
   ["parse correct line", test_parse_correct_line;
    "validate correct line", test_validate_correct_line;
    "validate missing me", test_validate_missing_me;
+   "validate empty cluster", test_validate_empty_cluster;
+   "validate me not member of cluster", test_validate_me_not_cluster_member;
   ]
   
 let test_suite =
