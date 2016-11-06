536382d4eb3d8b9eba91940a624855c5f4cab393
Karl Palmskog <palmskog@gmail.com>
2016-11-06 17:17:25 -0600

    serialization test for ClientResponse

--- extraction/vard/test/VarDSerializationTest.ml
+++ extraction/vard/test/VarDSerializationTest.ml
@@ -1,14 +1,24 @@
 open OUnit2
 open ListLabels
 open TestCommon
+open Util
 
 let tear_down () text_ctxt = ()
 
 let test_serialize_not_leader text_ctxt =
-  assert_equal ((1, 15), "NotLeader 1 15") (VarDSerialization.serialize (VarDRaft.NotLeader (1, 15)))
+  assert_equal ((2, 15), "NotLeader 2 15")
+    (VarDSerialization.serialize (VarDRaft.NotLeader (2, 15)))
+
+let test_serialize_client_response test_ctxt =
+  let o = VarDRaft.Response (char_list_of_string "awesome", None, None) in
+  assert_equal ((3, 34), "Response 3 34 awesome - -")
+    (VarDSerialization.serialize (VarDRaft.ClientResponse (3, 34, (Obj.magic o))))
   
 let test_list =
-  ["serialize NotLeader", test_serialize_not_leader]
+  [
+    "serialize NotLeader", test_serialize_not_leader;
+    "serialize ClientResponse", test_serialize_client_response
+  ]
 
 let tests =
   "VarDSerialization" >:::
