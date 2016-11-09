4c9d957b7e74df8f27fbd1289662c728a60add61
Karl Palmskog <palmskog@gmail.com>
2016-11-09 11:32:45 -0600

    reorganize Travis script, avoid polluting root dir with scripts

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -1,9 +1,9 @@
-pushd ..
-  opam init --yes --no-setup
-  eval $(opam config env)
-  opam repo add coq-released https://coq.inria.fr/opam/released
-  opam install coq coq-mathcomp-ssreflect ounit --yes
+opam init --yes --no-setup
+eval $(opam config env)
+opam repo add coq-released https://coq.inria.fr/opam/released
+opam install coq coq-mathcomp-ssreflect ounit --yes
 
+pushd ..
   git clone 'http://github.com/uwplse/StructTact'
   pushd StructTact
     ./build.sh
@@ -22,13 +22,13 @@ popd
 
 case $MODE in
   analytics)
-    ./analytics.sh
+    ./script/analytics.sh
     ;;
   vard-quick)
-    ./vard-quick.sh
+    ./script/vard-quick.sh
     ;;
   vard-test)
-    ./vard-test.sh
+    ./script/vard-test.sh
     ;;
   *)
     ./build.sh
