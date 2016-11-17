a2b8e54735d3568185b1b56c6e6543deebd8128f
Karl Palmskog <palmskog@gmail.com>
2016-11-16 20:11:36 -0600

    make Travis Coq and ssreflect versions configurable

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -1,7 +1,7 @@
 opam init --yes --no-setup
 eval $(opam config env)
 opam repo add coq-released https://coq.inria.fr/opam/released
-opam install coq.8.5.3 coq-mathcomp-ssreflect.1.6 ounit.2.0.0 --yes --verbose
+opam install coq.$COQ_VERSION coq-mathcomp-ssreflect.$SSREFLECT_VERSION ounit.2.0.0 --yes --verbose
 
 pushd ..
   git clone 'http://github.com/uwplse/StructTact'
