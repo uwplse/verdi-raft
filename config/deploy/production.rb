b7d910ecd5eea13bb6abde8fd53e8c01678e85b9
Karl Palmskog <palmskog@gmail.com>
2016-12-27 17:44:57 -0600

    deploy the deploy branch

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -32,6 +32,7 @@ server "discoberry02.duckdns.org", user: "pi", roles: %w{node}, name: "1"
 # http://capistranorb.com/documentation/getting-started/configuration/
 # Feel free to add new variables to customise your setup.
 
+set :branch, 'deploy'
 set :node_port, 6000
 set :client_port, 5000
 
