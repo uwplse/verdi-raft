ff810794194dea3010a79591f520773b9b53b51b
Karl Palmskog <palmskog@gmail.com>
2017-01-01 02:20:32 -0600

    fix vard command line error and help display

--- extraction/vard/ml/vard.ml
+++ extraction/vard/ml/vard.ml
@@ -4,13 +4,22 @@ open Str
 open Opts
 
 let _ =
-  let  _ = parse Sys.argv in
-
+  let _ =
+    try
+      parse Sys.argv
+    with
+    | Arg.Help msg ->
+      printf "%s: %s" Sys.argv.(0) msg;
+      exit 2
+    | Arg.Bad msg ->
+      eprintf "%s" msg;
+      exit 2
+  in
   let _ =
     try
       validate ()
     with Arg.Bad msg ->
-      eprintf "%s: %s." Sys.argv.(0) msg;
+      eprintf "%s: %s" Sys.argv.(0) msg;
       prerr_newline ();
       exit 2
   in
