d710f78920790366f9b7cebec6767f4d558a1509
Karl Palmskog <palmskog@gmail.com>
2017-01-12 21:58:54 -0600

    build vard using verdi-runtime, adjust configure script and coqproject.sh for OPAM use

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -1,24 +1,8 @@
 opam init --yes --no-setup
 eval $(opam config env)
 opam repo add coq-released https://coq.inria.fr/opam/released
-opam install coq.$COQ_VERSION coq-mathcomp-ssreflect.$SSREFLECT_VERSION ounit.2.0.0 uuidm.0.9.6 --yes --verbose
-
-pushd ..
-  git clone 'https://github.com/uwplse/StructTact.git'
-  pushd StructTact
-    ./build.sh $TARGET
-  popd
-
-  git clone 'https://github.com/DistributedComponents/InfSeqExt.git'
-  pushd InfSeqExt
-    ./build.sh $TARGET
-  popd
-
-  git clone 'https://github.com/uwplse/verdi.git'
-  pushd verdi
-    ./build.sh $TARGET
-  popd
-popd
+opam repo add distributedcomponents http://opam.distributedcomponents.net
+opam install coq.$COQ_VERSION coq-mathcomp-ssreflect.$SSREFLECT_VERSION StructTact verdi verdi-runtime ounit.2.0.0 --yes --verbose
 
 case $MODE in
   analytics)
