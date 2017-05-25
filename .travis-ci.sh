1530d830b7ac62d07400c2017b9e5ba17d4cf554
Karl Palmskog <palmskog@gmail.com>
2017-05-24 20:09:44 -0500

    OPAM files for Travis targets

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -7,22 +7,18 @@ opam repo add coq-released https://coq.inria.fr/opam/released
 opam repo add distributedcomponents-dev http://opam-dev.distributedcomponents.net
 
 opam pin add coq $COQ_VERSION --yes --verbose
-opam pin add coq-mathcomp-ssreflect $SSREFLECT_VERSION --yes --verbose
-opam install StructTact verdi --yes --verbose
 
 case $MODE in
-  analytics)
-    ./script/analytics.sh
+  proofalytics)
+    opam pin add verdi-raft-proofalytics . --yes --verbose
     ;;
   vard)
-    opam install verdi-runtime --yes --verbose
-    ./build.sh vard
+    opam pin add vard . --yes --verbose
     ;;
   vard-test)
-    opam install verdi-runtime ounit.2.0.0 --yes --verbose
-    ./build.sh vard-test
+    OPAMBUILDTEST=1 opam pin add vard . --yes --verbose
     ;;
   *)
-    ./build.sh $TARGET
+    opam pin add verdi-raft . --yes --verbose
     ;;
 esac
