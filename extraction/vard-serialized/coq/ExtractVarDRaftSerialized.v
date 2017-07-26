7102fbead067683b3574fca1fa64401259ae5d61
Justin Adsuara <justbads@cs.washington.edu>
2017-07-26 12:15:17 -0700

    remove vard-serialized/VarDRaft.ml

--- extraction/vard-serialized/coq/ExtractVarDRaftSerialized.v
+++ extraction/vard-serialized/coq/ExtractVarDRaftSerialized.v
@@ -12,6 +12,12 @@ Require Import Verdi.ExtrOcamlNatIntExt.
 Require Import Verdi.ExtrOcamlBool.
 Require Import Verdi.ExtrOcamlList.
 Require Import Verdi.ExtrOcamlFin.
+
+Require Import Cheerios.Cheerios.
 Require Import Cheerios.ExtrCheerios.
+
+Extract Inlined Constant nat_serialize => "(fun i -> Serializer_primitives.putInt (Int32.of_int i))".
+
+Extract Inlined Constant nat_deserialize => "(Serializer_primitives.map Int32.to_int Serializer_primitives.getInt)".
 
 Extraction "extraction/vard-serialized/ml/VarDRaftSerialized.ml" seq transformed_base_params transformed_multi_params transformed_failure_params.
