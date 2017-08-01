c81a7e3153ef884631ee0fb2c786bfb294b400ee
Karl Palmskog <palmskog@gmail.com>
2017-08-01 15:10:03 -0500

    use extraction-related files for vard-serialized instead of magical incantations

--- extraction/vard-serialized/coq/ExtractVarDRaftSerialized.v
+++ extraction/vard-serialized/coq/ExtractVarDRaftSerialized.v
@@ -1,4 +1,6 @@
 Require Import Verdi.Verdi.
+Require Import Cheerios.Cheerios.
+
 Require Import Verdi.VarD.
 Require Import VerdiRaft.VarDRaftSerialized.
 
@@ -11,18 +13,11 @@ Require Import Verdi.ExtrOcamlNatIntExt.
 
 Require Import Verdi.ExtrOcamlBool.
 Require Import Verdi.ExtrOcamlList.
-Require Import Verdi.ExtrOcamlFin.
+Require Import Verdi.ExtrOcamlFinInt.
 
-Require Import Cheerios.Cheerios.
-Require Import Cheerios.ExtrCheerios.
-
-Extract Inlined Constant nat_serialize => "(fun i -> Serializer_primitives.putInt (Int32.of_int i))".
-Extract Inlined Constant nat_deserialize => "(Serializer_primitives.map Int32.to_int Serializer_primitives.getInt)".
-
-Extract Inlined Constant fin_serialize => "(fun _ i -> Serializer_primitives.putInt (Int32.of_int i))".
-Extract Inlined Constant fin_deserialize => "(fun _ -> Serializer_primitives.map Int32.to_int Serializer_primitives.getInt)".
-
-Extract Inlined Constant ascii_serialize => "Serializer_primitives.putByte".
-Extract Inlined Constant ascii_deserialize => "Serializer_primitives.getByte".
+Require Import Cheerios.ExtrOcamlCheerios.
+Require Import Cheerios.ExtrOcamlCheeriosNatInt.
+Require Import Cheerios.ExtrOcamlCheeriosString.
+Require Import Cheerios.ExtrOcamlCheeriosFinInt.
 
 Extraction "extraction/vard-serialized/ml/VarDRaftSerialized.ml" seq transformed_base_params transformed_multi_params transformed_failure_params.
