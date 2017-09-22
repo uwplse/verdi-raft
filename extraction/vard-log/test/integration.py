89b9711d1b96314c790ff0889f33c2d40a6599ae
Justin Adsuara <justinadsuara@gmail.com>
2017-09-22 16:55:42 -0700

    Small test changes

--- extraction/vard-log/test/integration.py
+++ extraction/vard-log/test/integration.py
@@ -11,8 +11,7 @@ class TestVard(unittest.TestCase):
     client = None
     processes = None
 
-    def setUp(self):
-        """Start up a cluster"""
+    def startProcesses(self):
         self.processes = []
         for i in range(3):
             port = 8000 + i
@@ -27,16 +26,24 @@ class TestVard(unittest.TestCase):
             proc = subprocess.Popen(args, stdout=FNULL, stderr=subprocess.STDOUT, close_fds=True)
             self.processes.append(proc)
             time.sleep(1)
+
+    def setUp(self):
+        """Start up a cluster"""
+        self.startProcesses()
         cluster = [('localhost', 8000),
                    ('localhost', 8001),
                    ('localhost', 8002)]
         host, port = Client.find_leader(cluster)
         self.client = Client(host, port)
 
-    def tearDown(self):
+    def terminateProcesses(self):
         for i in range(3):
             self.processes[i].terminate()
-#            shutil.rmtree('db-%d' % i)
+
+    def tearDown(self):
+        self.terminateProcesses()
+        for i in range(3):
+            shutil.rmtree('db-%d' % i)
         self.client = None
         self.processes = None
 
@@ -48,8 +55,8 @@ class TestVard(unittest.TestCase):
        self.client.put('answer', '42')
        self.client.put('plse', 'lab')
        self.client.put('average', 'joe')
-       self.tearDown()
-       self.setUp()
+       self.terminateProcesses()
+       self.startProcesses()
        self.assertEqual(self.client.get('answer'), '42')
        self.assertEqual(self.client.get('plse'), 'lab')
        self.assertEqual(self.client.get('average'), 'joe')
