8896e0c43da4980c8fc71e450f03d2f5b22fe3b0
Karl Palmskog <palmskog@gmail.com>
2017-12-04 05:17:49 -0600

    adapt to new approach where client sends id, introduce client reconnect function

--- extraction/vard/ml/VarDSerialization.ml
+++ extraction/vard/ml/VarDSerialization.ml
@@ -5,35 +5,39 @@ open VarD
 open Util
 
 let serializeOutput out =
-  let (cid, outStr) = 
-    match (Obj.magic out) with
+  let (c, os) =
+    match Obj.magic out with
     | NotLeader (client_id, request_id) ->
        (client_id, sprintf "NotLeader %s" (string_of_int request_id))
     | ClientResponse (client_id, request_id, o) ->
-       let Response (k, value, old) = (Obj.magic o) in
+       let Response (k, value, old) = Obj.magic o in
        (client_id,
-        match (value, old) with
-        | Some v, Some o -> sprintf "Response %s %s %s %s"
-                                    (string_of_int request_id)
-                                    (string_of_char_list k)
-                                    (string_of_char_list v)
-                                    (string_of_char_list o)
-        | Some v, None -> sprintf "Response %s %s %s -"
-                                  (string_of_int request_id)
-                                  (string_of_char_list k)
-                                  (string_of_char_list v)
-        | None, Some o -> sprintf "Response %s %s - %s"
-                                  (string_of_int request_id)
-                                  (string_of_char_list k)
-                                  (string_of_char_list o)
-        | None, None -> sprintf "Response %s %s - -"
-                                (string_of_int request_id)
-                                (string_of_char_list k))
-  in (cid, Bytes.of_string outStr)
+        match value, old with
+        | Some v, Some o ->
+           sprintf "Response %s %s %s %s"
+             (string_of_int request_id)
+             (string_of_char_list k)
+             (string_of_char_list v)
+             (string_of_char_list o)
+        | Some v, None ->
+           sprintf "Response %s %s %s -"
+             (string_of_int request_id)
+             (string_of_char_list k)
+             (string_of_char_list v)
+        | None, Some o ->
+           sprintf "Response %s %s - %s"
+             (string_of_int request_id)
+             (string_of_char_list k)
+             (string_of_char_list o)
+        | None, None ->
+           sprintf "Response %s %s - -"
+             (string_of_int request_id)
+             (string_of_char_list k))
+  in (c, Bytes.of_string os)
 
 let deserializeInp i =
   let inp = String.trim (Bytes.to_string i) in
-  let r = regexp "\\([0-9]+\\) \\([A-Z]+\\) +\\([/A-za-z0-9]+\\|-\\) +\\([/A-za-z0-9]+\\|-\\) +\\([/A-za-z0-9]+\\|-\\)[^/A-za-z0-9]*" in
+  let r = regexp "\\([0-9]+\\) \\([A-Z]+\\) +\\([/A-Za-z0-9]+\\|-\\) +\\([/A-Za-z0-9]+\\|-\\) +\\([/A-Za-z0-9]+\\|-\\)[^/A-Za-z0-9]*" in
   if string_match r inp 0 then
     (match (matched_group 1 inp, matched_group 2 inp,
             matched_group 3 inp, matched_group 4 inp,
@@ -57,3 +61,6 @@ let deserializeInput inp client_id =
 let deserializeMsg (b : bytes) : VarDRaft.msg = Marshal.from_bytes b 0
 
 let serializeMsg (msg : VarDRaft.msg) : bytes = Marshal.to_bytes msg []
+
+let deserializeClientId b =
+  if Bytes.length b = 4 then Some (int_of_raw_bytes b) else None
